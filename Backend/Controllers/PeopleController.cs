006583a4f97a2bc252a43cac33170354c2752806
mac <damansa1@naver.com>
2026-05-06 17:50:09 +0900

    GET, POST 엔드포인트까지 처리완료

--- Backend/Controllers/PeopleController.cs
+++ Backend/Controllers/PeopleController.cs
@@ -1,5 +1,7 @@
+using Backend.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Controllers;
 
@@ -7,5 +9,62 @@ namespace Backend.Controllers;
 [ApiController]
 public class PeopleController : ControllerBase
 {
+    private readonly AppDbContext _context;
+
+    public PeopleController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddPerson(Person person)
+    {
+        try
+        {
+            _context.People.Add(person);
+            await _context.SaveChangesAsync();
+            return CreatedAtRoute("GetPerson", new { id = person.Id }, person); // 201 Created status code + person object in response body
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            // 500 Internal Server Error status code + exception message in response body
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetPeople()
+    {
+        try
+        {
+            var people = await _context.People.ToListAsync();
+            return Ok(people); // 200 Ok status code + list of people in response body
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            // 500 Internal Server Error status code + exception message in response body
+        }
+    }
+
+    [HttpGet("{id:int}", Name = "GetPerson")]
+    public async Task<IActionResult> GetPerson(int id)
+    {
+        try
+        {
+            var person = await _context.People.FindAsync(id);
+            if (person is null)
+            {
+                return NotFound(); // 404 Not Found status code
+            }
+            return Ok(person); // 200 Ok status code + person object in response body
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            // 500 Internal Server Error status code + exception message in response body
+        }
+    }
+    
 }
 
--- Backend/Program.cs
+++ Backend/Program.cs
@@ -6,7 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // services
 builder.Services.AddControllers();
 
-string connectionString = "Data Source = Person.db";
+string connectionString = builder.Configuration.GetConnectionString("Default") 
+    ?? throw new ArgumentException("Connection string is null");
 builder.Services.AddDbContext<AppDbContext>(op => op.UseSqlite(connectionString));
 
 var app = builder.Build();
