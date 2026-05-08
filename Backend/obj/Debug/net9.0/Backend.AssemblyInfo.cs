d04f86850b042a2a41bdb4ba4f30e6b180978b09
mac <damansa1@naver.com>
2026-05-08 14:22:33 +0900

    tailwind, lucide, Navbar.jsx 추가

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e93104e3bf4bff978713cd54107e4b91b68c939")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d1a27fed2750ab5f1183c90066acf39391ea8d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
